e1833868b00065c01eb6832645a0c8f1fbf8a80d
SHASHI KANT SINGH <162179698+shashikant1202@users.noreply.github.com>
2024-04-30 11:53:29 +0000

    some edition in comment

--- PATTERN/solidrectangle.c
+++ PATTERN/solidrectangle.c
@@ -2,11 +2,11 @@
 int main(){
     int m;
     int n;
-    printf("How many lines : ");
+    printf("Enter No. Of Rows : ");
     scanf("%d",&m);
-    printf("How many Stars you want to print in a single line : ");
+    printf("Enter No. of Column : ");
     scanf("%d",&n);
-    //*********.....mupto n no of stars
+    //*********.....upto n no of stars
     for(int i = 1; i<=m; i++){  //outer loop----> No of lines
     for (int i = 1; i <= n; i++)  // Inner loop----> No of stars in each line
     {
