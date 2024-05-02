e11ced088d73ab1c19613d9714200cf7b5f22be1
SHASHI KANT SINGH <162179698+shashikant1202@users.noreply.github.com>
2024-05-02 05:57:16 +0000

    ulta triangle

--- reversetriangularshapebystar.c
+++ reversetriangularshapebystar.c
@@ -4,8 +4,10 @@ int main(){
     printf("Enter no. of rows:- ");
     scanf("%d",&n);
     for(int i = n; i<=1; i++){  //no of lines / rows --> i
-        for(int j = 1; j<=n+1-i; j++){    //no of columns --> 
+    int a = n;
+        for(int j = 1; j<=a; j++){    // if i = 1, then must have j = 4
             printf("* ");
+            a--;
         }
         printf("\n");
     }
